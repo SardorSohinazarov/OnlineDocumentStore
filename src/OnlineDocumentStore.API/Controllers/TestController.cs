132cbfc0ae29826d372be7c90d70e7ed8143adb0
Sardor <sardorstudent0618@gmail.com>
2024-04-07 00:32:09 +0500

    Web api added

---
+++ src/OnlineDocumentStore.API/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineDocumentStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Qalesiz()
+            => Ok("Yaxshi rahmat");
+    }
+}
