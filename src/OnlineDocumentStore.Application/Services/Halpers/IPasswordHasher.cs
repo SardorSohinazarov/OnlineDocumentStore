3e62d6bf354fba594fd7c037512d018bc8b47944
Sardor <sardorstudent0618@gmail.com>
2024-04-10 22:07:53 +0500

    Password Hasher Service

---
+++ src/OnlineDocumentStore.Application/Services/Halpers/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineDocumentStore.Application.Services.Halpers
+{
+    public interface IPasswordHasher
+    {
+        string Encrypt(string password, string salt);
+        bool Verify(string hash, string password, string salt);
+    }
+}
