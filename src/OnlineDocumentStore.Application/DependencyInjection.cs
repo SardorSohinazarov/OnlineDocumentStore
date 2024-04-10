7a972af25cf01cfec3a7a595e953983a154179de
Sardor <sardorstudent0618@gmail.com>
2024-04-10 22:09:05 +0500

    DI

--- src/OnlineDocumentStore.Application/DependencyInjection.cs
+++ src/OnlineDocumentStore.Application/DependencyInjection.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using OnlineDocumentStore.Application.Services;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineDocumentStore.Application.Services.AuthServices;
+using OnlineDocumentStore.Application.Services.FileServices;
+using OnlineDocumentStore.Application.Services.Halpers;
+using OnlineDocumentStore.Application.Services.JWTServices;
+using OnlineDocumentStore.Application.Services.PDFFileServices;
+using OnlineDocumentStore.Application.Services.QRCodeServices;
 
 namespace OnlineDocumentStore.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
             services
+                .AddScoped<IAuthService, AuthService>()
                 .AddScoped<IFileService, FileService>()
+                .AddScoped<IPasswordHasher, PasswordHasher>()
                 .AddScoped<IPDFFileService, PDFFileService>()
-                .AddScoped<IQRCodeService, QRCodeService>();
+                .AddScoped<IQRCodeService, QRCodeService>()
+                .AddScoped<IJWTService, JWTService>();
+
+            services.AddHttpContextAccessor();
 
             return services;
         }
