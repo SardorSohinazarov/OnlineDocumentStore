11d8ca6093bafb7ab93159bd6d6721ba9763fd19
Sardor <sardorstudent0618@gmail.com>
2024-04-10 22:07:21 +0500

    Auth DTOs

---
+++ src/OnlineDocumentStore.Application/Models/Auth/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineDocumentStore.Application.Models.Auth
+{
+    public class RegisterDTO
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
