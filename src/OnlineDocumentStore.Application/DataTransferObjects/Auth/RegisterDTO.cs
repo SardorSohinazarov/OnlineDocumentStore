b0fcf06131396499ab3a38c74b3041dfe3faf599
Sardor <sardorstudent0618@gmail.com>
2024-04-12 00:30:30 +0500

    Ko'chirildi

--- src/OnlineDocumentStore.Application/DataTransferObjects/Auth/RegisterDTO.cs
+++ src/OnlineDocumentStore.Application/DataTransferObjects/Auth/RegisterDTO.cs
@@ -1,10 +1,11 @@
-﻿namespace OnlineDocumentStore.Application.Models.Auth
+﻿namespace OnlineDocumentStore.Application.DataTransferObjects.Auth
 {
     public class RegisterDTO
     {
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
--- src/OnlineDocumentStore.Application/DataTransferObjects/Auth/TokenDTO.cs
+++ src/OnlineDocumentStore.Application/DataTransferObjects/Auth/TokenDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace OnlineDocumentStore.Application.Models.Auth
+﻿namespace OnlineDocumentStore.Application.DataTransferObjects.Auth
 {
     public class TokenDTO
     {
